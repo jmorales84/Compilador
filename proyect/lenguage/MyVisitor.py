54c6544156b5e2e82d759ff1dcf51b3fb11e52f4
jmorales84 <jessicavegamorales8@gmail.com>
2025-11-14 09:15:10 -0600

    modificacion1

--- proyect/lenguage/MyVisitor.py
+++ proyect/lenguage/MyVisitor.py
@@ -6,7 +6,7 @@ class MyVisitor(GrammarVisitor):
     def __init__(self):
         self.memory = { }
 
-    # Visita nuestro programa
+    #Visita nuestro programa
     def visitProgram(self, ctx: GrammarParser.ProgramContext):
         for stmt in ctx.statement():
             self.visit(stmt)
@@ -21,7 +21,6 @@ class MyVisitor(GrammarVisitor):
         # Se obtiene el valor, ya sea un valor numerico o una expresion
         value=self.visit(ctx.expr())
 
-
         # Se almacena en memoria a partir del nombre y el valor
         # self.memory[name]=value
 
@@ -32,22 +31,21 @@ class MyVisitor(GrammarVisitor):
         if var_type == 'string' and not isinstance(value, str):
             raise TypeError(f"Error en '{name}' ")
 
-        # Se almacena en memoria el valor y su tipo
+        # Almacena en memoria el valor y su tipo
         self.memory[name] = {'value': value, 'type': var_type}
 
         # Se verifica que la variable exista
         if name not in self.memory:
             raise NameError(f"Variable '{name}' no ha sido definida.")
 
-        # Se obtiene el tipo original de la variable 
+        # Se obtiene el tipo original de la variable
         var_type = self.memory[name]['type']
-
-        # Se valida que el nuevo valor coincida con el nuevo tipo original
-        if (var_type == 'int' and not isinstance(value, int )) or \
-            (var_type == 'string' and not isinstance(value, str)):
+        # Se valida que el nuevo valor coincida con el tipo original
+        if (var_type == 'int' and not isinstance(value, int)) or \
+           (var_type == 'string' and not isinstance(value, str)):
             raise TypeError(f"No se puede asignar un {type(value).__name__} ")
 
-        # Se actualiza el valor en memoria 
+        # Se actualiza el valor en memoria
         self.memory[name]['value'] = value
 
     # Definimos la impresion
@@ -58,28 +56,27 @@ class MyVisitor(GrammarVisitor):
         print(value)
 
     # Definimos el visitor para el if
-    def visiIf_statement(self, ctx:GrammarParser.If_statementContext):
+    def visitIf_statement(self, ctx:GrammarParser.If_statementContext):
         # Buscamos la expresion
         condition = self.visit(ctx.expr())
-
-        # Evaluamos si existe la condicion para que ejecute el bloque 
+        # Evaluamos si existe la condicion para que ejecute el bloque
         if condition:
             self.visit(ctx.block())
         return None
 
     # Definimos el visitor para el for
     def visitFor_statement(self, ctx:GrammarParser.For_statementContext):
-        self.visit(ctx.assign(0)) # Inicializador 
+        self.visit(ctx.assing(0)) # Inicializador
         # Evaluamos la expresion para el for
         while self.visit(ctx.expr()):
             self.visit(ctx.block())
-            self.visit(ctx.assign(1)) # Incremento
+            self.visit(ctx.assing(1))  # Incremento
 
     # Definir el bloque para evaluarlo
     def visitBlock(self, ctx:GrammarParser.BlockContext):
-        # Evalue las instrucciones dentro del bloque 
+        #Evalue las instrucciones dentro del bloque
         for stmt in ctx.statement():
-            # Visita cada una
+            #Visita cada una
             self.visit(stmt)
         return None
 
@@ -92,12 +89,12 @@ class MyVisitor(GrammarVisitor):
             # Si el nombre de la variable no esto, lanza un error
             if name not in self.memory:
                 #raise NameError(f"Variable '{name}' no definida")
-                # Si existe el nombre retorna la variable
+                #Si existe el nombre retorna la variable
                 #return self.memory[name]
-                
+
                 raise NameError(f"Variable '{name}' no ha sido definida")
             # Si existe el nombre retorna su valor
-            return self.memory[name]['value']
+            return self.memory[name]["value"]
         
         # Busca si es un numero
         elif ctx.NUMBER():
@@ -123,10 +120,9 @@ class MyVisitor(GrammarVisitor):
             if ctx.op.text == "/":
                 #Verifica la division de cero
                 if right == 0:
-                    raise ValueError("Division por cero")
-                return left / right
-
-            # Evaluacion de operadores de comparacion
+                    raise ValueError("Division por cero") 
+                return left / right 
+            #Evalucion de operadores de comparacion
             if ctx.op.text == ">":
                 return left > right
             if ctx.op.text == "<":
